045c576579147938ffac6499da4f0325e0749cf0
PedroEniacSt <142404351+PedroEniacSt@users.noreply.github.com>
2023-10-18 09:01:18 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,28 +3,52 @@
 using namespace std;
 
 class animal{
-    public:
+public:
     int comida;
     int quantidade;
     float peso;
 };
+animal a;
+
+void functionCalc() {
+
+    cout << a.comida * a.quantidade / 30;
+
+}
+
+bool animalIsValid = true;
+
 int main() {
     animal a;
     string nome;
-    
+
+
     cout << "animal: ";
     cin >> nome;
-    
-    if(nome == "leopardo"){
-        a.comida = 10;
-        a.quantidade = 5;
-        a.peso = 15;
-        cout << a.peso << endl << a.comida << endl << a.quantidade;
-    } else {
-        cout << "invalido";
-        
-        return 0;
+    do {
+        if (animalIsValid == false) {
+            cout << "please enter a valid animal: ";
+            cin >> nome;
+        } 
+        if (nome == "leopardo") {
+            a.comida = 30;
+            a.quantidade = 5;
+            cout << 444;
+
+        } if (nome == "macaco") {
+            a.comida = 30;
+            a.quantidade = 5;
+            a.peso = 15;
+            cout << &functionCalc;
+            return 0;
+        }
+
     }
-    
+    while (!animalIsValid);
+
+
+
+
+
     return 0;
 }
