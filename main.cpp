6b1554af3aaaf524b4f0ca4e998b2299d4c05337
PedroEniacSt <142404351+PedroEniacSt@users.noreply.github.com>
2023-10-24 08:45:23 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,53 @@ using namespace std;
 class animal{
 public:
     int comida;
+    int quantidade;#include <iostream>
+
+using namespace std;
+
+class animal{
+public:
+    int comida;
     int quantidade;
+    int kilo;
+    double price;
+};
+animal a;
+
+
+bool animalIsValid;
+
+int main() {
+    string nome;
+
+
+    cout << "animal: ";
+    cin >> nome;
+    animalIsValid = false;
+    do{
+         if (nome == "Leopardo") {
+             animalIsValid = true;
+             cout << "Please enter the NUMBERS of Animals: ";
+             cin >> a.quantidade;
+             a.comida = 50;
+             a.kilo = 5;
+             int x = a.comida / a.quantidade;
+             int calc = x / 30;
+             cout << "Quantity of Food per Animal: " << x;
+             cout << ""
+
+
+            } else {
+                cout << "please enter a animal valid: ";
+                cin >> nome;
+        }
+    } 
+
+
+
+    while(animalIsValid == false);
+}
+
     float peso;
 };
 
