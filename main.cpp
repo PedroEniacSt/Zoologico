bfd04687bb968d1da57ff1ff8408008a332ea72c
PedroEniacSt <142404351+PedroEniacSt@users.noreply.github.com>
2023-10-24 19:32:27 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,6 @@ using namespace std;
 class animal{
 public:
     int comida;
-    int quantidade;#include <iostream>
-
-using namespace std;
-
-class animal{
-public:
-    int comida;
     int quantidade;
     int kilo;
     double price;
@@ -20,63 +13,100 @@ animal a;
 
 
 bool animalIsValid;
+bool repeat;
 
 int main() {
     string nome;
+    string verify;
 
 
-    cout << "animal: ";
-    cin >> nome;
-    animalIsValid = false;
-    do{
-         if (nome == "Leopardo") {
-             animalIsValid = true;
-             cout << "Please enter the NUMBERS of Animals: ";
-             cin >> a.quantidade;
-             a.comida = 50;
-             a.kilo = 5;
-             int x = a.comida / a.quantidade;
-             int calc = x / 30;
-             cout << "Quantity of Food per Animal: " << x;
-             cout << ""
 
+    repeat = true;
+    while(repeat){
+        cout << "There's some of animals that we have in our Zoo: "<< endl;
+        cout << "__________________________" << endl;
+        cout << "|         ANIMALS        |" << endl;
+        cout << "|       ----------       |" << endl;
+        cout << "|Leopardo, Jabuti, Macaco|" << endl;
+        cout << "|       ----------       |" << endl;
+        cout << "|________________________|" << endl;
+        cout << "Which animal you want insert data?: ";
+        cin >> nome;
+        animalIsValid = false;
+        do{
+            if (nome == "Leopardo" || nome == "leopardo") {
+                animalIsValid = true;
+                cout << "Please enter the NUMBERS of Animals: ";
+                cin >> a.quantidade;
+                a.price = 3000,00;
+                a.comida = 500;
+                a.kilo = 5;
+                int x = a.comida / a.quantidade;
+                const int y = a.comida * a.kilo / 30;
+                int calc = a.comida / a.quantidade * a.price;
+                cout << "Animal: Leopardo" << endl;
+                cout << "Quantity of Food per Animal: 5" << endl;
+                cout << "Quantity of Food per Day: " << x << endl;
+                cout << "Quantity of Food per Month: " << y << endl;
+                cout << "Price Calculeted per Month: " << calc << endl;
+                cout << "Price Estimated: 30000" << endl;
 
+                repeat = false;
+            } else if (nome == "Macaco" || nome == "macaco") {
+                animalIsValid = true;
+                cout << "Please enter the NUMBERS of Animals: ";
+                cin >> a.quantidade;
+                a.price = 1500,00;
+                a.comida = 300;
+                a.kilo = 5;
+                int x = a.comida / a.quantidade;
+                const int y = a.comida * a.kilo / 30;
+                int calc = a.comida / a.quantidade * a.price;
+                cout << "Animal: Leopardo" << endl;
+                cout << "Quantity of Food per Animal: 5" << endl;
+                cout << "Quantity of Food per Day: " << x << endl;
+                cout << "Quantity of Food per Month: " << y << endl;
+                cout << "Price Calculeted per Month: " << calc << endl;
+                cout << "Price Estimated: 15000" << endl;
+
+                repeat = false;
+            } else if (nome == "Jabuti" || nome == "jabuti") {
+                animalIsValid = true;
+                cout << "Please enter the NUMBERS of Animals: ";
+                cin >> a.quantidade;
+                a.price = 800,00;
+                a.comida = 250;
+                a.kilo = 5;
+                int x = a.comida / a.quantidade;
+                const int y = a.comida * a.kilo / 30;
+                int calc = a.comida / a.quantidade * a.price;
+                cout << "Animal: Leopardo" << endl;
+                cout << "Quantity of Food per Animal: 5" << endl;
+                cout << "Quantity of Food per Day: " << x << endl;
+                cout << "Quantity of Food per Month: " << y << endl;
+                cout << "Price Calculeted per Month: " << calc << endl;
+                cout << "Price Estimated: 15000" << endl;
+
+                repeat = false;
             } else {
                 cout << "please enter a animal valid: ";
                 cin >> nome;
+            }
+            cout << "Want continue? (Y = Yes / No = Press any key): ";
+            cin >> verify;
+            if (verify == "Y" || verify == "y"){
+                repeat = true;
+                break;
+            }
         }
-    } 
 
 
 
-    while(animalIsValid == false);
-}
 
-    float peso;
-};
+        while(animalIsValid == false);
 
-bool animalIsValid;
-
-int main() {
-    animal a;
-    string nome;
-
-
-    cout << "animal: ";
-    cin >> nome;
-    animalIsValid = false;
-    do{
-         if (nome == "leopardo"){
-             animalIsValid = true;
-             cout << "bimbimbambam";
-
-            } else {
-                cout << "please enter a animal valid: ";
-                cin >> nome;
-        }
-    } 
+    }
 
 
 
-    while(animalIsValid == false);
 }
